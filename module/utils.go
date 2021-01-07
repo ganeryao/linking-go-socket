7be62d8c0406bcdd5f01dc9eddeacfa517dee4db
YaoWeiXin <419576308@qq.com>
2021-01-07 14:09:28 +0800

    JoinGroup、LeaveGroup、ClearGroup方法提取到工具包内

--- module/utils.go
+++ module/utils.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ganeryao/linking-go-agile/common"
 	"github.com/ganeryao/linking-go-agile/errors"
 	"github.com/ganeryao/linking-go-agile/protos"
+	"github.com/ganeryao/linking-go-socket/manager"
 	"github.com/topfreegames/pitaya"
 	"github.com/topfreegames/pitaya/util"
 	"reflect"
@@ -48,4 +49,53 @@ func SendRPC(ctx context.Context, serverId string, route string, request *protos
 	if err != nil {
 		logger.Errorf("Failed to execute RPCTo %s - %s", route, err.Error())
 	}
+}
+
+/**
+加入组
+*/
+func JoinGroup(ctx context.Context, group string, uid string) bool {
+	logger := manager.GetLog(ctx)
+	// 1、判断用户是否已经在组中，如果不存在再加入
+	flag, err := pitaya.GroupContainsMember(ctx, group, uid)
+	if err != nil {
+		logger.Error("Failed to contains group member: " + err.Error())
+		return false
+	}
+	if !flag {
+		err = pitaya.GroupAddMember(ctx, group, uid)
+		if err != nil {
+			logger.Error("Failed to join group: " + err.Error())
+			return false
+		}
+	}
+	return true
+}
+
+/**
+离开组
+*/
+func LeaveGroup(ctx context.Context, group string, uid string) bool {
+	logger := manager.GetLog(ctx)
+	// 1、用户从组中移除
+	err := pitaya.GroupRemoveMember(ctx, group, uid)
+	if err != nil {
+		logger.Error("Failed to leave group member: " + err.Error())
+		return false
+	}
+	return true
+}
+
+/**
+清空组
+*/
+func ClearGroup(ctx context.Context, group string) bool {
+	logger := manager.GetLog(ctx)
+	// 1、清空组中成员
+	err := pitaya.GroupRemoveAll(ctx, group)
+	if err != nil {
+		logger.Error("Failed to clear group: " + err.Error())
+		return false
+	}
+	return true
 }
