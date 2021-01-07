3f58f640cd9af66b426b2f0f214d27b58e84d4c0
YaoWeiXin <419576308@qq.com>
2021-01-07 18:16:54 +0800

    修改ConvertHandlerMsg方法

--- common/converts.go
+++ common/converts.go
@@ -4,7 +4,6 @@ import (
 	lkCommon "github.com/ganeryao/linking-go-agile/common"
 	lkError "github.com/ganeryao/linking-go-agile/errors"
 	"github.com/ganeryao/linking-go-agile/protos"
-	"github.com/ganeryao/linking-go-socket/constants"
 	"github.com/ganeryao/linking-go-socket/module"
 	"strings"
 )
@@ -24,10 +23,5 @@ func ConvertApi(api string) string {
 func ConvertHandlerMsg(request *protos.LRequest, uid string, data interface{}) (*module.HandlerMsg, *lkError.Error) {
 	request.Api = ConvertApi(request.GetApi())
 	lkCommon.ParseJson(request.Param, data)
-	flag := module.ContainsHandler(request.GetApi())
-	if !flag {
-		// 不是支持的api请求，直接抛弃
-		return nil, lkError.NewError(constants.ErrUnsupportedRequest, lkError.ErrBadRequestCode)
-	}
 	return &module.HandlerMsg{Uid: uid, ApiType: module.ApiModeMain, Api: request.Api, Msg: data}, nil
 }
--- pojo/dto/join_dto.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type JoinDTO struct {
-	// 用户编号
-	Uid string `json:"uid"`
-}
